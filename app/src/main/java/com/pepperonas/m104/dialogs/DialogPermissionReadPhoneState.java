aa03703f9b6a6e1805d21c05cf208b17d8d21922
martin <martinpaush@gmail.com>
2019-04-08 23:55:01 +0200

    - updated gradle - updated copyright

--- app/src/main/java/com/pepperonas/m104/dialogs/DialogPermissionReadPhoneState.java
+++ app/src/main/java/com/pepperonas/m104/dialogs/DialogPermissionReadPhoneState.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Martin Pfeffer
+ * Copyright (c) 2019 Martin Pfeffer
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
